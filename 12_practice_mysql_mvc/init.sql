c294567792ab62a09f841fb5069705bd2e7a73ce
yhs0329 <scar0329@naver.com>
2024-02-16 14:30:03 +0900

    2.16. 회원가입과 로그인 DB연동 연습

--- 12_practice_mysql_mvc/init.sql
+++ 12_practice_mysql_mvc/init.sql
@@ -1,3 +1,4 @@
+-- Active: 1707208765864@@127.0.0.1@3306@sesac
 -- 데이터베이스 목록 확인
 SHOW databases;
 
@@ -11,7 +12,12 @@ SHOW tables;
 DROP TABLE IF EXISTS user;
 
 -- TODO: user 데이터베이스 생성
-
+CREATE TABLE user(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userid VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    pw VARCHAR(20) NOT NULL
+)
 -- user 데이블 데이터 추가
 INSERT INTO user (userid, name, pw) VALUES ('allie', 'allie', '1234');
 INSERT INTO user (userid, name, pw) VALUES ('test', 'test', '1234');
