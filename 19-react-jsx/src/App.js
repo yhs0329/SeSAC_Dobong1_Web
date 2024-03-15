94fd8810946b192ccc684ee6c4a8ca2cf02ab216
yhs0329 <scar0329@naver.com>
2024-03-15 16:44:14 +0900

    3.15. 리액트

---
+++ 19-react-jsx/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import "./App.css";
+import UseJSX from "./UseJSX";
+import PracticeJSX from "./PracticeJSX";
+
+const title = "Hello World";
+function App() {
+  return (
+    <div>
+      <h1>JSX 문법을 배워봐요</h1>
+      <UseJSX></UseJSX>
+      <PracticeJSX></PracticeJSX>
+    </div>
+  );
+}
+
+export default App;
---
+++ 19-react-jsx/src/PracticeJSX.js
@@ -0,0 +1,26 @@
+// 함수형 컴포넌트로
+let name = "탐비";
+let animal = "진돗개";
+let a = 6;
+let b = 5;
+
+export default function prac1() {
+  return (
+    <div>
+      <h2>제 반려동물의 이름은 {name}입니다.</h2>
+      <h2>
+        {name}는 {animal}입니다.
+      </h2>
+      <div>{3 + 5 === 8 ? "정답입니다." : "오답입니다."}</div>
+      <br></br>
+      <div>{a > b && "a가 b보다 큽니다."}</div>
+      <br></br>
+      <div className="prac5">
+        <h1 className="prac4">Hello World</h1>
+        아이디 : <input type="text"></input>
+        <br></br>
+        비밀번호 : <input type="password"></input>
+      </div>
+    </div>
+  );
+}
